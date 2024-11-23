e274460b232306db00466bfef93eedd22716c00c
ivanova anastasija <ivanova.anastasiia@phystech.edu>
2024-11-23 18:53:44 +0300

    mini test

---
+++ src/test/java/ru/greencat/CatTest.java
@@ -0,0 +1,11 @@
+package ru.greencat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+    @Test
+    public void someTest() {
+        Assertions.assertEquals("1", "1");
+    }
+}
