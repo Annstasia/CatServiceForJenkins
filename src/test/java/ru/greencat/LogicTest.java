8574a446177fdf2266c2522fd86bc2e538749e2b
ivanova anastasija <ivanova.anastasiia@phystech.edu>
2024-11-24 19:05:00 +0300

    fix test

--- src/test/java/ru/greencat/LogicTest.java
+++ src/test/java/ru/greencat/LogicTest.java
@@ -7,7 +7,8 @@ public class LogicTest {
     private final CatLogic logic = new CatLogic();
     @Test
     public void testLogic() {
-        Assertions.assertEquals(0, logic.logic(0));
+        Assertions.assertEquals(6, logic.logic(0));
         Assertions.assertEquals(6, logic.logic(1));
+        Assertions.assertEquals(3, logic.logic(6));
     }
 }
